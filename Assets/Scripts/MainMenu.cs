6b41aa9a9ac7430de88c8f8cbd11ee5145e58a17
Sabrina <lwy0100@gmail.com>
2021-07-21 15:53:58 +0800

    Copy MainMenu page, import Vuforia and model target

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartAR()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartWorkspace()
+    {
+        SceneManager.LoadScene("ARWorkSpace");
+    }
+
+    public void StartMADCam()
+    {
+        SceneManager.LoadScene("MADCamera");
+    }
+
+    public void QuitApp()
+    {
+        Debug.Log("Quit button clicked");
+        Application.Quit();
+    }
+}
